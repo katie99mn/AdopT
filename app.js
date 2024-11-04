d299e0eb513b79a4b0ec0f3b8c0ed8526ea6e3b7
Katie <kswong3@wisc.edu>
2024-11-03 22:25:20 -0600

    added java for the buttons on click

---
+++ app.js
@@ -0,0 +1,41 @@
+function r_e(id) {
+  return document.querySelector(`#${id}`);
+}
+// sign in JS
+let signinbtn = r_e("signinbtn");
+let signinmod = r_e("si-mod");
+let signinbg = r_e("si-bg");
+let signinform = r_e("si-form");
+
+signinbtn.addEventListener("click", () => {
+  signinmod.classList.add("is-active");
+});
+
+signinbg.addEventListener("click", () => {
+  signinmod.classList.remove("is-active");
+});
+
+// sign up in JS
+let signupbtn = r_e("signupbtn");
+let signupmod = r_e("su-mod");
+let signupbg = r_e("su-bg");
+let signupsubmit = r_e("su-submit");
+let signupform = r_e("su-form");
+
+signupbtn.addEventListener("click", () => {
+  signupmod.classList.add("is-active");
+});
+
+signupbg.addEventListener("click", () => {
+  signupmod.classList.remove("is-active");
+});
+
+// sign out code
+let signoutbtn = r_e("signoutbtn");
+signoutbtn.addEventListener("click", () => {
+  // test event listener works
+  // display message to let user know they signed out
+  auth.signOut().then(() => {
+    configure_message_bar("You are now logged out!");
+  });
+});
