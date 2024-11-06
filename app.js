8efe3c76a37c28563b82d7c56d7d4606dac3f94c
Katelyn <katelynqlam@gmail.com>
2024-11-06 00:07:28 -0600

    Add event form button  and edit sign in/out

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 function r_e(id) {
   return document.querySelector(`#${id}`);
 }
+
+//add event js
+let add_btn = r_e("add_btn");
+let add_mod = r_e("add-mod");
+let add_bg = r_e("add-bg");
+let event_form = r_e("event-form");
+
+add_btn?.addEventListener("click", () => {
+  add_mod.classList.add("is-active");
+});
+add_bg?.addEventListener("click", () => {
+  add_mod.classList.remove("is-active");
+});
+
 // sign in JS
 let signinbtn = r_e("signinbtn");
 let signinmod = r_e("si-mod");
 let signinbg = r_e("si-bg");
 let signinform = r_e("si-form");
 
-signinbtn.addEventListener("click", () => {
+signinbtn?.addEventListener("click", () => {
   signinmod.classList.add("is-active");
 });
 
-signinbg.addEventListener("click", () => {
+signinbg?.addEventListener("click", () => {
   signinmod.classList.remove("is-active");
 });
 
@@ -22,17 +36,17 @@ let signupbg = r_e("su-bg");
 let signupsubmit = r_e("su-submit");
 let signupform = r_e("su-form");
 
-signupbtn.addEventListener("click", () => {
+signupbtn?.addEventListener("click", () => {
   signupmod.classList.add("is-active");
 });
 
-signupbg.addEventListener("click", () => {
+signupbg?.addEventListener("click", () => {
   signupmod.classList.remove("is-active");
 });
 
 // sign out code
 let signoutbtn = r_e("signoutbtn");
-signoutbtn.addEventListener("click", () => {
+signoutbtn?.addEventListener("click", () => {
   // test event listener works
   // display message to let user know they signed out
   auth.signOut().then(() => {
