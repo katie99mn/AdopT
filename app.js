868f6b0f01a5322237d39a130fa883a9c03cb2e5
dtthai <dtthai2@wisc.edu>
2024-11-06 17:58:50 -0600

    added js for event form

--- app.js
+++ app.js
@@ -15,6 +15,32 @@ add_bg?.addEventListener("click", () => {
   add_mod.classList.remove("is-active");
 });
 
+// adding an event to a collection
+document.querySelector("#event_submit").addEventListener("click", () => {
+  let name = document.querySelector("#event_name").value;
+  let location = document.querySelector("#event_location").value;
+  let time = document.querySelector("#event_time").value;
+  let type = document.querySelector("#event_type").value;
+  let description = document.querySelector("#event_description").value;
+  let upload = document.querySelector("#image_upload").value;
+
+  let event = {
+    name: name,
+    location: location,
+    time: time,
+    type: type,
+    description: description,
+    upload: upload,
+  };
+
+  db.collection("events")
+    .add(event)
+    .then(() => {
+      alert("New event added!");
+      // show_people();
+    });
+});
+
 // sign in JS
 let signinbtn = r_e("signinbtn");
 let signinmod = r_e("si-mod");
