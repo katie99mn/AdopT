6d2a88810eb1ad28092f70cb91ebbb5aa54fde27
Ava Feider <af10712014@gmail.com>
2024-11-24 13:02:03 -0600

    add home page file upload to db

--- app.js
+++ app.js
@@ -497,3 +497,28 @@ r_e("learn_more").addEventListener("click", () => {
 });
 
 // HOME PAGE IMG UPLOAD
+document
+  .getElementById("home_upload_img")
+  .addEventListener("change", function () {
+    let file = this.files[0];
+    if (!file) return;
+
+    let imagename = new Date().getTime() + "_" + file.name;
+    const image = firebase.storage().ref("event_images/" + imagename);
+    const task = image.put(file);
+
+    task
+      .then((snapshot) => {
+        return snapshot.ref.getDownloadURL();
+      })
+      .then((url) => {
+        const upcomingeventdata = {
+          image: url,
+          uploadedAt: firebase.firestore.FieldValue.serverTimestamp(),
+        };
+        firebase
+          .firestore()
+          .collection("upcoming_events")
+          .add(upcomingeventdata);
+      });
+  });
