185f7638238f0c86f673b0de32db563df99b0406
Alec <adstowell@wisc.edu>
2024-11-25 20:51:56 -0600

    Changed some innerHTML Events Formatting

--- app.js
+++ app.js
@@ -383,20 +383,36 @@ function renderEvents(isAdmin) {
         eventDiv.classList.add("columns", "mt-2");
 
         eventDiv.innerHTML = `
-          <div class="column is-6 ml-3 mr-2">
+          <div class="column is-4 ml-4">
             <img src="indeximages/a1.png" class="smaller_image" alt="Event" />
           </div>
-          <div class="column is-6 mr-2">
+          <div class="column is-7 mt-4 mr-4">
             <div class="event-details">
-              <p class="is-size-2">${event.name}</p>
-              <p class="is-size-3">Location: ${event.location}</p>
-              <p class="is-size-3">Time: ${event.time}</p>
-              <p class="is-size-3">Type: ${event.type}</p>
+              <p class="is-size-3">${event.name}</p>
+              <p class="is-size-4">Location: ${event.location}</p>
+              <p class="is-size-4">Time: ${event.time}</p>
+              <p class="is-size-4">Type: ${event.type}</p>
               <br />
               <p class="is-size-5">${event.description}</p>
             </div>
           </div>
         `;
+
+        // Previous code
+        // eventDiv.innHTML = `<div class="columns mt-2">
+        //   <div class="column is-5 ml-4">
+        //     <img src="indeximages/a1.png" class="smaller_image" alt="Event" />
+        //   </div>
+        //   <div class="column is-7 mt-4 mr-4">
+        //     <p class="is-size-3">${event.data().name}</p><br />
+        //     <p class="is-size-4">Location: ${event.data().location}</p>
+        //     <p class="is-size-4">Date: ${formattedDate}</p>
+        //     <p class="is-size-4">Time: ${formattedTime}</p>
+        //     <p class="is-size-4">Type: ${event.data().type}</p>
+        //     <br />
+        //     <p class="is-size-5">${event.data().description}</p>
+        //   </div>
+        // </div>`;
 
         // Add delete button if the user is an admin
         if (isAdmin) {
