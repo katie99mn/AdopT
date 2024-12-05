7fc7f60f55a911c61d494ce91c8b77ad68f81788
prestonnphann <ptphan@wisc.edu>
2024-12-05 15:44:53 -0600

    format and add new delete btn

--- app.js
+++ app.js
@@ -129,7 +129,13 @@ function show_events(isAdmin) {
               <div class="column is-7">
                 <p class="is-size-3 darkbrown has-text-weight-bold">${
                   eventData.name
-                }</p><br />
+                }<span>
+               <button class="button learn_btn deletebtn is-pulled-right ${
+                 isAdmin ? "" : "is-hidden"
+               }" onclick="deleteEvent('${event.id}')">
+                 X
+               </button>
+             </span></p><br />
                 <p class="is-size-5 darkbrown">Location: ${
                   eventData.location
                 }</p>
